5dcef597f9b173aaa05b251af51257c37d410c65
Nicholas <nskg@pm.me>
2022-05-25 14:00:41 +0200

    clean up for stylized facts project

--- tests/test_make_trade_bars.py
+++ tests/test_make_trade_bars.py
@@ -4,15 +4,15 @@ from adp.make_intraday_trade_bars import make_intraday_trade_bars
 class TestCase(unittest.TestCase):
     def test_read_trades_write_bars(self):
         for symbol in ["ABC", "DEF", "GHI"]:
-            test_args = ["tests/test_data/"+symbol +"_NYSE@0_Matching-MatchedOrders.csv.gz",
-                         "tests/test_data/"+symbol +"_NYSE@0_daily_bars.csv.gz"]
+            test_args = ["test_data/"+symbol +"_NYSE@0_Matching-MatchedOrders.csv.gz",
+                         "test_output/"+symbol +"_NYSE@0_daily_bars.csv.gz"]
             make_trade_bars(test_args)
 
     def test_read_trades_write_intraday_bars(self):
         for symbol in ["ABC", "DEF", "GHI" ]:
             test_args = ["10T",
-                         "tests/test_data/" + symbol + "_NYSE@0_Matching-MatchedOrders.csv.gz",
-                         "tests/test_data/" + symbol + "_NYSE@0_10T_bars.csv.gz"]
+                         "test_data/" + symbol + "_NYSE@0_Matching-MatchedOrders.csv.gz",
+                         "test_output/" + symbol + "_NYSE@0_10T_bars.csv.gz"]
             make_intraday_trade_bars(test_args)
 
 
